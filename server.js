95e977676d5e55c59928faa48dd65ca31c16d080
ddobson001 <david.m.dobson@hotmail.com>
2019-07-24 15:28:56 -0400

    add

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, './public/style.css')));
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "./views/Index.html"));
+  res.sendFile(path.join(__dirname, './app/views/index.html'));
 });
 
 app.post('/populateLqDb1', (req, res) => {
